73c9bd3a99d9c8344e63f7a051c349e8530a6432
Jaurock <aurockjsoe@gmail.com>
2016-06-04 13:09:33 -0600

    inicio menu

--- app/src/main/java/com/example/jrock/mywarehouse/MainActivity.java
+++ app/src/main/java/com/example/jrock/mywarehouse/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.jrock.mywarehouse;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
 }
