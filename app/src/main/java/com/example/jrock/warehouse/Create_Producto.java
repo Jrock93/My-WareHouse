c4a3e36606f44ef24629c735c2bee67c169f76e6
Jaurock <aurockjsoe@gmail.com>
2016-06-09 18:00:37 -0600

    mejora

---
+++ app/src/main/java/com/example/jrock/warehouse/Create_Producto.java
@@ -0,0 +1,13 @@
+package com.example.jrock.warehouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Create_Producto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create__producto);
+    }
+}
---
+++ app/src/main/java/com/example/jrock/warehouse/DBAlmacen.java
@@ -0,0 +1,7 @@
+package com.example.jrock.warehouse;
+
+/**
+ * Created by Jrock on 7/6/2016.
+ */
+public class DBAlmacen {
+}
